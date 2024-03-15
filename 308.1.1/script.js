a44f11babdea25735e9d8ab3824d719321156ffb
CataQu <cataquarleri@gmail.com>
2024-03-15 16:38:58 -0600

    completed lab 308.1.1

--- 308.1.1/script.js
+++ 308.1.1/script.js
@@ -4,24 +4,9 @@ const n2 = 15;
 const n3 = 20;
 const n4 = 5;
 
-//Numbers divisible by 5
-const divisibleBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0;
-console.log("Divisible by 5 " + divisibleBy5);
 
-//First number is larger than the last
-const firstLarger = n1 > n4
-console.log("First larger " + firstLarger)
-
-// Accomplish the following arithmetic chain:
-// Subtract the first number from the second number.
-// Multiply the result by the third number.
-// Find the remainder of dividing the result by the fourth number.
-const arithmeticOperation = (n2 - n1) * n3 % n4 == 0
-console.log("Arithmetic operation " + arithmeticOperation)
-
-//Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
-const isNotOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-
+//INITIAL CODE
+// ------------------------------------------------------------
 // Check one: add up to 50
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
@@ -48,7 +33,7 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique && divisibleBy5 && firstLarger;
+const isValid = isSum50 && isTwoOdd && isOver25 && isUnique && divisibleBy5 && firstLarger;
 
 // Finally, log the results.
 console.log(isValid);
@@ -61,3 +46,51 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+  //MY CODE 
+
+  //Numbers divisible by 5
+const divisibleBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0;
+console.log("All numbers divisible by 5 " + `That is ${divisibleBy5}`);
+
+//First number is larger than the last
+const firstLarger = n1 > n4
+console.log("The first number larger than the first one:" + `That statement is ${firstLarger}`)
+
+// Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+// Multiply the result by the third number.
+// Find the remainder of dividing the result by the fourth number.
+const arithmeticOperation = (n2 - n1) * n3 % n4 == 0
+console.log("If I substract the first number from the second number and multiply the result by the third number and then divide the result by the fourth, the remainder of the operation is " + arithmeticOperation)
+
+//Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+const isNotOver25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
+console.log("All the numbers are below 25:" + `The statement is ${isNotOver25}`)
+
+const isValid2 = isSum50 && isTwoOdd && isOver25 && isUnique && divisibleBy5 && firstLarger;
+console.log(`The four numbers are valid accodring to the provided criteria:  ${isValid2}`)
+
+//PART 2: PRACTICAL MATH
+
+const tripTotal = 1500 // miles
+const costPerGallon = 3 //dollars
+const budget = 175 //dollars
+
+let speed = 75 //mph - Change manually as we are not yet using conditionals
+let milesPerGallon = 23 //miles - Change manually as we are not yet using conditionals
+
+let gallonsNeeded = tripTotal / milesPerGallon
+console.log(`If I go at ${speed}mph I will need ${gallonsNeeded} gallons`)
+
+let moneyExpend = gallonsNeeded * costPerGallon;
+let enoughBudget = moneyExpend <= budget;
+console.log(`If I go at ${speed}mph my money is enough. This is ${enoughBudget}`)
+
+let timeSpent = tripTotal / speed //hours
+console.log(`If I go at ${speed}mph my trip will take ${timeSpent} hours`)
+
+
+
+
